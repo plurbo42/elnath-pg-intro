a06dacfebe040677fb47cb9b7d864d3ebdc47707
Paige Scheunemann <pls927@gmail.com>
2017-11-21 10:25:43 -0600

    sending hardcoded array

--- server/server.js
+++ server/server.js
@@ -6,7 +6,15 @@ var app = express();
 var port = 5000;
 
 app.use(express.static('server/public'));
-app.use(body-parser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
+
+var shoes = [{name: 'nike', cost: '75'}]
+
+app.get('/shoes', function(req, res){
+    res.send(shoes);
+});
+
+//for localhost 5000/shoes - should return array of shoe objects
 
 app.listen(port, function(){
     console.log('listening on port ', port);
