2f553798c413942ed3d19d0ae374465f8644cfd6
Paige Scheunemann <pls927@gmail.com>
2017-11-21 13:04:14 -0600

    full stack get request

--- server/server.js
+++ server/server.js
@@ -8,11 +8,38 @@ var port = 5000;
 app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-var shoes = [{name: 'nike', cost: '75'}]
-
 app.get('/shoes', function(req, res){
-    res.send(shoes);
+    //attempt to connect to database
+    pool.connect(function(errorConnectingToDatabase, client, done){
+        if(errorConnectingToDatabase){
+            //There was an error connecting to the database
+            console.log('error connecting to database', errorConnectingToDatabase);
+            res.sendStatus(500);
+        }else{
+            //we connected to the database. Let's get things from the database
+            client.query('SELECT * FROM shoes;', function(errorMakingQuery, result){
+                done();
+                if(errorMakingQuery){
+                    console.log('query failed ', errorMakingQuery)
+                    res.sendStatus(500);
+                }else{
+                    res.send(result.rows);
+                }
+            })
+        }
+    });
 });
+
+//pg configuration below
+var config = {
+    database: 'shoe_store',
+    host: 'localhost',
+    port: 5432,
+    max: 10, //how many connections at one time
+    idleTimeoutMilles: 30000, //
+}
+
+var pool = new pg.Pool(config)
 
 //for localhost 5000/shoes - should return array of shoe objects
 
